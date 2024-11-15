452d6136f888251ff5fa2c5686f6e841a7b965ef
Saptarshi samanta <79819563+yoursaptarshi@users.noreply.github.com>
2024-11-16 01:55:02 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -33,6 +33,9 @@ app.use(fileUpload({
 app.use("/api/v1",userRoutes);
 app.use("/api/v1",carRoutes);
 app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.get('/api/docs', (req, res) => {
+  res.redirect('https://www.postman.com/yoursaptarshi/ac38f2a0-3f6c-4ee2-85bb-a3dcc2b821fe/documentation/06zu50e/car-management-system');
+});
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
 });
