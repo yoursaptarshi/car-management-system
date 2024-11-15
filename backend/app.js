6566059818de04111d2975f3e611c4b8c5fb7a05
Saptarshi samanta <79819563+yoursaptarshi@users.noreply.github.com>
2024-11-16 00:44:28 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -33,7 +33,7 @@ app.use(fileUpload({
 
 app.use("/api/v1",userRoutes);
 app.use("/api/v1",carRoutes);
-
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
 });
